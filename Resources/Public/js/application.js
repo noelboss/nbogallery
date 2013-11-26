caa5164f561f79cb6a96fd65b718a7e30eafe90e
Noël Bossart <noel.bossart@namics.com>
2013-11-26 23:25:24 +0100

    Attempt to fix lightbox

--- Resources/Public/js/application.js
+++ Resources/Public/js/application.js
@@ -5,32 +5,32 @@
 (function($){
 	$(document).ready(function(){
 		var $mod = $('.tx-nbogallery');
-		
+
 		if($mod.length < 1){
 			return;
 		}
-		
+
 		var $b = $('body');
 		$('.fancybox-thumbs', $mod).fancybox({
 			prevEffect : 'none',
 			nextEffect : 'none',
-			
+
 			topRatio: 0.9,
-			
-			maxHeight: $b.height()-150,
+
+			//maxHeight: $b.height()-150,
 
 			closeBtn  : false,
 			arrows    : true,
 			nextClick : true,
-			
-			fitToView : true,
+
+			fitToView : true/*,
 
 			helpers : {
 				thumbs : {
 					width  : 50,
 					height : 50
 				}
-			}
+			}*/
 		});
 	});
 })(jQuery);
